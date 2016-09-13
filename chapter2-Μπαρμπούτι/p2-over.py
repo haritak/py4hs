2443e13b37bc3ac115efaa36364e177e4658d580
Charitakis Yannis <github@ych.gr>
2016-09-13 05:59:22 +0300

    Θεωρία 2ου

---
+++ chapter2-Μπαρμπούτι/p2-over.py
@@ -0,0 +1,35 @@
+import random
+
+print ("Πάτα έντερ για να πέσουν τα ζάρια", end=" ")
+input()
+
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+
+roll = dice1 + dice2
+print( "Εριξες", dice1, dice2, "=", roll)
+
+if roll==7 or roll==11:
+    print("Κέρδισες με την πρώτη!")
+elif roll<=3 or roll==12:
+    print("..έχασες με την πρώτη")
+else:
+    over=False
+    while not over:
+    print("Ξαναρίξε! Πρέπει να φέρεις", roll)
+    print ("Πάτα έντερ για να πέσουν τα ζάρια", end=" ")
+    input()
+
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+
+    newroll = dice1 + dice2
+    print( "Εριξες", dice1, dice2, "=", roll)
+    if newroll==roll:
+        print("Κέρδισες!")
+        over = True
+    elif newroll==7:
+        print("Εχασες")
+        over = True
+
+
