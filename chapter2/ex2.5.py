431ee0a0b85e4ae5c37609a8c47e85c4cc97474a
Charitakis Yannis <github@ych.gr>
2016-09-29 20:07:54 +0300

    Added Manos solution and logo

--- chapter2/ex2.5.py
+++ chapter2/ex2.5.py
@@ -1,17 +1,23 @@
+#WG37 - Ηρακλείου
 import random
 
 while True:
-    card1 = random.randint(1,11)
-    card2 = random.randint(card1+2,13)
-    d = card2-card1
+    load=True
+    while load:
+        card1 = random.randint(1,13)
+        card2 = random.randint(1,13)
+        d = card2-card1
+        if d>1 or d<-1:
+            load=False
+    if d<0:
+        d=d*-1    
     mult = 12/d
-
+    print("Οι αριθμοί είναι ", card1, card2)
     bet = int(input("Πόσο στοιχηματίζεις; "))
-    c = int(input("Τί επιλέγεις; "))
+    c=random.randint(1,13)
+    print("Ο αριθμός σου είναι ", c)
     price = bet*mult
-    if c>card1 and c<card2:
+    if ((c>card1 and c<card2) or (c>card2 and c<card1)):
         print("Κέρδισες ", price)
     else:
-        print("Εχασες. Οι αριθμοί ήταν ", card1, card2,
-                "και θα κέρδιζες ", price)
-
+        print("Εχασες. Οι αριθμοί ήταν ", card1, card2, "και θα κέρδιζες ", price)
