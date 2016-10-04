de0ec4510850278fe2647e7a5a648fa872815ba7
Charitakis Yannis <github@ych.gr>
2016-10-04 06:45:46 +0300

    Ολόκληρο το παράδειγμα της θεωρίας

--- chapter3/p5.py
+++ chapter3/p5.py
@@ -1,20 +1,36 @@
 import random
+def midNumber(a,b):
+    print('Μάντεψε τον αριθμό', a,"-",b)
+    num = (a+b)//2
+    print("Ο υπολογιστής επιλέγει:", num)
+    return num
 
-secret = random.randint(1,32)
+def readNumber(a,b):
+    print('Μάντεψε τον αριθμό', a,"-",b)
+    num=int(input())
+    return num
+
+
+low=1
+high=32
+
+secret = random.randint(low, high)
 #ΓΙΑ ΕΛΕΓΧΟ : εμφάνιση μυστικού αριθμού
 print(secret)
 
 found = False
 tries = 4
 while not found and tries>=0:
-    print('Μάντεψε τον αριθμό 1 εως 32')
-    number=int(input())
+    #number=readNumber(low, high)
+    number=midNumber(low, high)
     if number>secret:
         print("Λάθος, είναι μικρότερος")
+        high = number-1
         print("Απομένουν", tries, "προσπάθειες")
         tries-=1
     elif number<secret:
         print("Λάθος, είναι μεγαλύτερος")
+        low=number+1
         print("Απομένουν", tries, "προσπάθειες")
         tries-=1
     else:
---
+++ chapter3/p5.rb
@@ -0,0 +1,44 @@
+def midNumber(a,b)
+    puts "Μάντεψε τον αριθμό #{a}-#{b}"
+    num = (a+b)/2
+    puts "Ο υπολογιστής επιλέγει: #{num}"
+    return num
+end
+
+def readNumber(a,b)
+    puts "Μάντεψε τον αριθμό #{a}-#{b}"
+    num=gets.to_i
+end
+
+
+low=1
+high=32
+
+secret = rand high + low 
+#ΓΙΑ ΕΛΕΓΧΟ : εμφάνιση μυστικού αριθμού
+puts(secret)
+
+found = false
+tries = 4
+while not found and tries>=0
+    #number=readNumber(low, high)
+    number=midNumber(low, high)
+    if number>secret
+        puts "Λάθος, είναι μικρότερος"
+        high = number-1
+        puts "Απομένουν #{tries} προσπάθειες"
+        tries-=1
+    elsif number<secret
+        print("Λάθος, είναι μεγαλύτερος")
+        low=number+1
+        puts "Απομένουν #{tries} προσπάθειες"
+        tries-=1
+    else
+        puts("Σωστά!")
+        #άμεση έξοδος απο την επανάληψη
+        found = true
+    end
+end
+if not found
+    puts("Ηταν ο", secret)
+end
