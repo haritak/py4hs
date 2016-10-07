70eb6d98cd5ba091a80cae38d60d04eb1d900235
Charitakis Yannis <github@ych.gr>
2016-10-07 06:03:53 +0300

    Δουλεύει για 6 και κάτω

--- chapter4/ex4.7.py
+++ chapter4/ex4.7.py
@@ -30,12 +30,25 @@ def readMatches(p,m):
 def randomMatches(m):
     return random.randint(1,maxMatches(m))
 
-def computeMatches(m):
-    mod=m%4
-    if mod==0:
-        return 1
+def computeMatches(m, mym):
+    if mym%2==1:
+        if m==1 or m==3:
+            return m
+        elif m==2 or m==4:
+            return m-1
+        elif m==6:
+            return 3
+        else:
+            return randomMatches(m)
     else:
-        return mod
+        if m==1 or m==2:
+            return m
+        elif m==3:
+            return 2
+        elif m==4 or m==5:
+            return 4
+        else:
+            return randomMatches(m)
 
 def announceWinner(matchesGathered,computer):
     if matchesGathered[computer]%2 ==0:
@@ -58,8 +71,8 @@ matchesGathered = [0,0,0]
 while matches>0:
     print("Σπίρτα που απομένουν:", matches)
     if player==computer:
-        removed = randomMatches(matches)
-        #removed = computeMatches(matches)
+        #removed = randomMatches(matches)
+        removed = computeMatches(matches, matchesGathered[computer])
         print("Ο υπολογιστής πήρε", removed, "σπίρτα")
     else:
         removed=readMatches(player,matches)
