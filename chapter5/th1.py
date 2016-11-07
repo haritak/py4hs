39a5c6a993a1e589d65522751c53db6f576abb25
Charitakis Yannis <github@ych.gr>
2016-11-07 18:36:37 +0200

    Refactoring based on theory

--- chapter5/th1.py
+++ chapter5/th1.py
@@ -1,4 +1,6 @@
 #
+import random
+
 planets = ["Ερμής", "Αφροδίτη", "Γη", "Αρης", "Δίας", "Κρόνος",
         "Ουρανός", "Ποσειδώνας"]
 
@@ -11,11 +13,53 @@ def addPluto():
 
     return answer in "yYΝν"
 
-if addPluto():
-    planets.append("Πλούτωνας")
+def select(nbChoices, possible, correct):
+    '''
+    nbChoices:πλήθος απαντήσεων που θα επιλεχθούν
+    possible:λίστα όλων των πιθανών απαντήσεων 
+    correct: σωστή απάντηση
+    '''
+
+    answers = random.sample(possible, nbChoices)
+    if correct not in answers:
+        index = random.randint(0, nbChoices-1)
+        answers[index] = correct
+
+    return answers
 
 
-import random
+def showMultiple(answers):
+    number=1
+    for answer in answers:
+        print("(", number, ") ", answer, sep="", end= " ")
+        number+=1
+
+def readAnswer(nbChoices):
+    while True:
+        choice = int(input())
+        if choice<1 or choice>nbChoices:
+            print("μη επιτρεπτός αριθμός")
+        else:
+            break
+
+    return choice
+
+
+def multipleChoice(nbChoices, possible, correct):
+    '''
+    nbChoices: πλήθος απαντήσεων που θα εμφανιστούν
+    possible: λίστα όλων των πιθανών απαντήσεων
+    correct: η σωστή απάντηση
+    '''
+    answers = select(nbChoices, possible, correct)
+    showMultiple(answers)
+    playerChoice= readAnswer(nbChoices)
+    if answers[playerChoice-1] == correct:
+        print("Μπράβο το βρήκες")
+    else:
+        print("Η σωστή απάντηση είναι ", correct, ".")
+        
+
 
 def findByName(planets):
     nbPlanets = len(planets)
@@ -32,10 +76,6 @@ def findByName(planets):
         print ("Ο πλανήτης ", planet, " είναι ο ", position+1,
                 "ος πλανήτης.", sep="")
 
-
-print("Ερώτηση 1:")
-findByName(planets)
-
 def findByPosition(planets):
     nbPlanets = len(planets)
     position = random.randint(0, nbPlanets-1)
@@ -43,15 +83,15 @@ def findByPosition(planets):
 
     print("Ποιός είναι ο ", position+1, "ος πλανήτης μετά τον Ηλιο;", 
             sep="")
-    answer = input()
-    if answer.lower().strip() == planet.lower():
-        print( "Μπράβο!" )
-    else:
-        print( "Είναι ο πλανήτης ", planet, ".", sep="")
+    multipleChoice(4, planets, planet)
 
 
+
+if addPluto():
+    planets.append("Πλούτωνας")
+
+print("Ερώτηση 1:")
+findByName(planets)
 
 print("Ερώτηση 2:")
 findByPosition(planets)
-
-
