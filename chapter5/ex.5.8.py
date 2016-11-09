cabce4353c33c34fc53222e2c10a4d8b0d642caa
Charitakis Yannis <github@ych.gr>
2016-11-09 07:24:03 +0200

    Πρόσθεσα και την δυνατότητα να παίζει μόνος του

--- chapter5/ex.5.8.py
+++ chapter5/ex.5.8.py
@@ -1,5 +1,38 @@
 import random
 
+#0 Terminates
+#1 Gives a hint
+
+#playAlone = False
+playAlone = True
+
+def suggestMove(L):
+
+    #Start from the right side
+    #and spot the first out of order number
+    i=9
+    while i>1 and L[i-1] == i:
+        i-=1
+
+    if i==0:
+        return 0
+
+    target = i
+    #debugging : 
+    #print("Out of order is number ", target)
+
+    #locate target in the list
+    target_index = L.index( target )+1
+    #debugging : 
+    #print("Which is at place ", target_index)
+
+    #if it's on the first position
+    #just flip the list
+    if target_index==1:
+        return target
+
+    #bring it to the first
+    return target_index
 
 def showList(L):
     for i in L:
@@ -36,9 +69,14 @@ random.shuffle( L )
 rounds = 0
 while not inOrder(L):
     showList(L)
-    how_many = getInput()
+    if playAlone:
+        how_many = suggestMove(L)
+    else:
+        how_many = getInput()
     if how_many==0:
         break
+    if how_many==1:
+        print(suggestMove(L))
 
 
     if how_many>1:
