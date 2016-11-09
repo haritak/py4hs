b79bced5aff92144267a1e58b99826049ad420e4
Charitakis Yannis <github@ych.gr>
2016-11-09 08:10:33 +0200

    Fixed a small bug

--- chapter5/ex.5.10.py
+++ chapter5/ex.5.10.py
@@ -4,17 +4,18 @@ lines = []
 
 for l in range(0, n):
     current_line = [1]
-    for y in range(1,l):
-        if y==l-1:
+    for y in range(1,l+1):
+        if y==l:
             current_line.append(1)
         else:
             previous_line = lines[l-1]
-            current_line.append( 
-                    previous_line[y-1]+
-                    previous_line[y])
+            new = previous_line[y-1]+previous_line[y]
+            current_line.append( new )
 
+    print(current_line)
     lines.append(current_line)
 
+print("----")
 for l in lines:
     for i in l:
         print(i, sep=" ", end=" ")
